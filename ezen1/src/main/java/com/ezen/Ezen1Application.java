00f529a57886de02189983da91fb0fd307d78e24
Hms200 <96504521+Hms200@users.noreply.github.com>
2022-02-18 11:31:23 +0900

    build project

---
+++ ezen1/src/main/java/com/ezen/Ezen1Application.java
@@ -0,0 +1,13 @@
+package com.ezen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ezen1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ezen1Application.class, args);
+	}
+
+}
---
+++ ezen1/src/test/java/com/ezen/Ezen1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ezen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ezen1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
