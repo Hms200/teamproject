7931ee5c0d41649b0e8325a4d3650d2d94d0664a
Hms200 <96504521+Hms200@users.noreply.github.com>
2022-02-21 11:57:16 +0900

    main js작성, html ref에 main js 링크, 팝업화면 JS작성, notice.html작성

---
+++ mock_html/js/main.js
@@ -0,0 +1,36 @@
+// popup hide & show   
+function popupHideAndShow(target) {
+    const searchBar = document.getElementById(String(target));
+    if(searchBar.classList.contains("d-none")){
+        searchBar.classList.add("d-block");
+        searchBar.classList.remove("d-none");
+    }else if(searchBar.classList.contains("d-block")){
+        searchBar.classList.add("d-none");
+        searchBar.classList.remove("d-block");
+    }
+};
+
+// 장바구니 벳지 장바구니가 0이면 숨기기
+///로딩시.  ajax통신으로 실시간 반영될 경우 ajax 코드에서 해당 함수 호출되게 만들것.
+window.onload(bedgeHideAndShow());
+function bedgeHideAndShow() {
+    const bedge = document.getElementById('bedge');
+    const bedgeNumber = document.getElementById('bedgeNumber').innerText;
+    if(bedgeNumber == '0'){
+        bedge.classList.remove("d-block");
+        bedge.classList.add("d-none");
+
+    }else{
+        bedge.classList.add("d-block");
+        bedge.classList.remove("d-none");
+    } 
+};
+
+//  카테고리 슬라이드 업
+$(function(){
+    var hideContainer = $("#cat_slideup");
+    $("#slide_toggle").click(function(){
+            hideContainer.slideToggle(600);
+        });
+})
+    
