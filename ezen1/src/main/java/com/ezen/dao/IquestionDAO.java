5bdee1fb0bd99f9698456743f95792f98d751b00
cotober7856 <cotober7856@naver.com>
2022-03-02 17:11:10 +0900

    DAO 파라미터 수정

--- ezen1/src/main/java/com/ezen/dao/IquestionDAO.java
+++ ezen1/src/main/java/com/ezen/dao/IquestionDAO.java
@@ -9,7 +9,7 @@ import com.ezen.dto.Question;
 @Mapper
 public interface IquestionDAO {
 	//goods_idx를 받아 MALL_QUESTION에서 해당 idx를 가진 모든 data 가져옴
-	public ArrayList<Question> getQuestionList(int goods_idx);
+	public ArrayList<Question> getQuestionList(String goods_idx);
 	//Question을 받아 MALL_QUESTION에 insert
 	public int insertQna(Question question);
 }
