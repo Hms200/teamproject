b4b4a698cae24641234b23790734d9eb2e11f665
cotober7856 <cotober7856@naver.com>
2022-03-02 17:43:39 +0900

    DAO 수정

--- ezen1/src/main/java/com/ezen/dao/InoticeDAO.java
+++ ezen1/src/main/java/com/ezen/dao/InoticeDAO.java
@@ -11,9 +11,9 @@ public interface InoticeDAO {
 	//Notice를 받아 MALL_NOTICE에 insert
 	public int insertNotice(Notice notice);
 	//해당 notice_idx 를 갖는 row를 받은 파라미터값으로 update
-	public int updateNotice(int notice_idx);
+	public int updateNotice(String notice_idx);
 	//MALL_NOTICE의  공지목록 가져옴
 	public ArrayList<Notice> getNoticeLIst();
 	//MALL_NOTICE의 해당 notice_idx를 갖는 공지 가져옴
-	public Notice getNotice(int notice_idx);
+	public Notice getNotice(String notice_idx);
 }
