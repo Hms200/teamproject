0ccd9bdf71b8b1778f4b14c968921fa63c1f14fe
Hms200 <96504521+Hms200@users.noreply.github.com>
2022-03-03 12:14:13 +0900

    Update MainController.java

--- ezen1/src/main/java/com/ezen/controller/MainController.java
+++ ezen1/src/main/java/com/ezen/controller/MainController.java
@@ -38,7 +38,7 @@ public class MainController {
 		String filelocation = fileService.fileUploader("thumb", file);
 		System.out.println(filelocation);
 		if(filelocation.charAt(0) == 'f') {
-			return "script:alert('파일업로드에 실패하였습니다.'); location.href='main'";     
+			return "<script>alert('파일업로드에 실패하였습니다.'); location.href='main'</script>";     
 		}
 		return "<script> location.href='main'</script>";
 	}
