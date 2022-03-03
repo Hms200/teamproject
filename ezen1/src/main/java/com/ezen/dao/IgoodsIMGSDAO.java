464d65e13b24609fc50f8505c1201de7ae260f90
Hms200 <96504521+Hms200@users.noreply.github.com>
2022-03-03 10:13:09 +0900

    goodsimgsDao 수정

--- ezen1/src/main/java/com/ezen/dao/IgoodsIMGSDAO.java
+++ ezen1/src/main/java/com/ezen/dao/IgoodsIMGSDAO.java
@@ -4,12 +4,14 @@ import java.util.ArrayList;
 
 import org.apache.ibatis.annotations.Mapper;
 
+import com.ezen.dto.GoodsIMGS;
+
 @Mapper
 public interface IgoodsIMGSDAO {
 
 	// 해당 goods_idx를 가진 모든 행의 goods_img를 가져옴
 	public ArrayList<String> getGoodsImgs(int goods_idx);
 	
-	// 해당 goods_idx를 가진 행에 Img 저장 경로를 insert
-	public int insertGoodsImgs(int goods_idx, String goods_img);
+	// GoodsIMGS를 insert
+	public int insertGoodsImgs(GoodsIMGS goodsImgs);
 }
