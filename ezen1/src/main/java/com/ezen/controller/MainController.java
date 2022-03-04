c41ee6ad98d179c21c8c0b95543f092c4a58fd1b
Hms200 <96504521+Hms200@users.noreply.github.com>
2022-03-04 21:20:09 +0900

    Hms notice 완성 (#69)
    * header js미작성
    
    * 레퍼런스페이지 수정
    
    * html ref 재수정
    
    * header.html 완성
    
    * header 수정, nav 완성( 링크미지정)
    
    * footer 완성, ref파일 수정
    
    * scroll to top 버튼 작성
    
    * header, footer, nav, main, 목업메인, ref html수정, js추가, custom.css수정
    
    * main js작성, html ref에 main js 링크, 팝업화면 JS작성, notice.html작성
    
    * notice.html 완성 / main.js 수정 / custom.css 수정
    
    custom.css   pagenation color 변경
    main.js  슬라이드업 main.html 안으로 옮김
    
    * Update notice.html
    
    * notice.html 완성, custom css 버튼색상문제 수정
    
    * notice.html 팝업창 별도페이지로 제작 예정
    
    * main.js 수정
    
    popupHideAndShow 함수 수정
    
    * notice.html 수정
    
    오타, 주석 수정
    
    * custom css 수정, stock.html 작성
    
    custom css btn-primary focus 외곽선 컬러반영 안되는것 수정
    
    stock.html 작성. 상품리스트 카드 완성되면 JS와 버튼 수정예정
    
    * Create transaction.html
    
    * transaction 작성중
    
    * css btn-secondary 수정, 페이지 상단 마진 수정, transaction.html 작성
    
    notice, stock, transaction의 상단 마진을 60px로 변경.
    <header와 결합할 경우 header와 겹치는 문제 해결>
    
    * Update header.html
    
    margin-bottom: 60px
    
    * transactionpop.html 작성중
    
    * Update transactionpop.html
    
    * transactionpop.html 완성, custom css 수정, transaction.html 수정
    
    custom.css에 var(--secondary)추가
    transaction.html 에 잘못 된 마진값 삭제
    
    * eventConfig.html 완성
    
    * nullChecker 함수 작성
    
    * 잘못된 마진값 수정
    
    * multi submit form의 submit 버튼에 적용할 함수 작성
    
    * Update main.js
    
    * notice, stock, transaction. transactionpop js 수정
    
    * header 검색창 placeholder 변경, nullcheck 반영
    
    * stock.html 검색창 nullcheck, placeholder 수정
    
    * DTO 작성
    
    * mybatis Mapper 작성
    
    * DAO 작성
    
    * contorller 기본파일 작성
    
    * service 기본파일 작성
    
    * .
    
    * Update custom.css
    
    * Delete 1 2.txt
    
    * fixed
    
    * fixed
    
    * fixed
    
    * main, nav, header, footer jsp변환 및 변환참고용예시파일작성
    
    * 프로젝트에 css, js 파일 추가
    
    * example.jsp 에 main.js반영 안된것 수정
    
    * .
    
    * fixed
    
    * fixed
    
    * fixed
    
    * stock.html jsp 변환
    
    * purchaselist 이름 수정 -> purchase
    
    * file upload service 작성
    
    * admin/goods 수정완료
    
    * Update memberListpopup.html
    
    * IgoodsDAO 작성
    
    * IgoodsIMGSDAO, IgoodsOptionDAO, IreviewIMGSDAO 작성
    
    * .
    
    * file uploader 작동상 문제 해결, main.jsp에 태스트용 폼 생성
    
    * fileService 주석오타 수정, goodsDAO, goodsMapper 작성
    
    * goodsIMGSDAO, goodsImgsMapper 작성
    
    * goods option mapper 작성
    
    * goodsimgDao 수정
    
    * goodsimgsDao 수정
    
    * review img DAO, mapper 작성
    
    * Delete web.xml
    
    * main controller 해당페이지 jsp 변환 완료
    
    * admin controller / card   jsp 변환
    
    * 사용자페이지 링크수정
    
    * fileuploader 에 파일명생성기능 추가
    
    * Update MainController.java
    
    * Update FileService.java
    
    * Create Pagenation.java
    
    * main.jsp 와 카드들 레이아웃 수정
    
    * main.jsp 용 공지사항기능 작성
    
    * main 화면 전체상품리스트카드 수정
    
    평상시 노출되는 모습과 검색결과를 노출하는 모습이 다르게 나옴.
    
    * entireItem.Card 완성 / 나머지 카드들 레이아웃 수정
    
    * 메인화면, 공지사항 작성
    
    * Update NoticeMapper.xml
    
    * Update MainController.java
    
    * Update Pagenation.java
    
    * Update NoticeMapper.xml
    
    * notice 관리자기능,사용자기능 분리
    
    * notice 페이지 완성

--- ezen1/src/main/java/com/ezen/controller/MainController.java
+++ ezen1/src/main/java/com/ezen/controller/MainController.java
@@ -1,17 +1,18 @@
 package com.ezen.controller;
 
-import java.awt.PageAttributes.MediaType;
 
-import javax.servlet.http.HttpSession;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+
 
 
 import com.ezen.dto.Notice;
@@ -24,6 +25,7 @@ public class MainController {
 	
 	@Autowired
 	MainService mainService;
+	
 
 	@RequestMapping("")
 	public String root() {
@@ -87,5 +89,4 @@ public class MainController {
 		String result = mainService.deleteNotice(notice_idx);
 		return result;
 	}
-
 }
