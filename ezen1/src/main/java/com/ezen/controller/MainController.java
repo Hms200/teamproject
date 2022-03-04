55ff291d22ec083f45b59ce51416ea6fb41b7cfc
Hms200 <96504521+Hms200@users.noreply.github.com>
2022-03-04 18:28:16 +0900

    Update MainController.java

--- ezen1/src/main/java/com/ezen/controller/MainController.java
+++ ezen1/src/main/java/com/ezen/controller/MainController.java
@@ -1,8 +1,6 @@
 package com.ezen.controller;
 
-import java.awt.PageAttributes.MediaType;
 
-import javax.servlet.http.HttpSession;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,11 +10,10 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.multipart.MultipartFile;
+
 
 
 import com.ezen.dto.Notice;
