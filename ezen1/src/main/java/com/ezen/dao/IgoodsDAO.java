155b9402ddb0e4c47a7c2dd70d841f71f0dd6752
Jun981020 <flwnsgud@naver.com>
2022-03-04 16:57:15 +0900

    commit
    commit

--- ezen1/src/main/java/com/ezen/dao/IgoodsDAO.java
+++ ezen1/src/main/java/com/ezen/dao/IgoodsDAO.java
@@ -25,7 +25,7 @@ public interface IgoodsDAO {
 	public ArrayList<Goods> getGoodsListByOnEvent(String onEvent);
 	
 	// 등록된 모든 상품 정보 가져오기
-	public ArrayList<Goods> getGoodsList();
+	public ArrayList<Goods> getAllGoodsList();
 
 	public ArrayList<Goods> getGoodsList(int start, int end);
 
