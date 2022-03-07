237f7e3763b9ee4a66ca6198a87a0ae46e20d476
DevYun97 <95176118+DevYun97@users.noreply.github.com>
2022-03-07 16:59:11 +0900

    Ydh (#74)
    * .
    
    * Revert "Apply .gitignore"
    
    This reverts commit 15b2ab7b25b3f6d167ed4e6d5b6f8bdcae7084a1.
    
    * Revert "."
    
    This reverts commit a760d0cd190954d4d25e71b27e5ccddb227883c8.
    
    * 타이틀 하단 선 추가 리뷰카드 마진,패딩 약간 수정
    
    코드 약간 수정합니다.
    
    * 유저/리뷰/1대1문의 DAO 작성
    
    * 1대1문의 수정
    
    * 회원탈퇴 jsp미완성
    
    * 유저, 리뷰,일대일 mapper작성, 리뷰 insert 추가 / 미테스트
    
    미테스트
    
    * 유저매퍼일부수정/회원가입Ajax연결
    
    로그인 만드는중. 회원가입 아이디체크 연결했지만 nullCheck랑 분리되어있어 아이디체크없이 회원가입이 가능한 문제발생 = 중복된 아이디로 가입 가능. 아직 미해결
    
    * 로그인idx 추가
    
    유저매퍼수정있어서 우선 올립니다
    
    * 회원가입작업중
    
    * lombok
    
    * 로그인기능_로그인시 테스트 페이지로 넘어감
    
    로그인시 테스트 페이지로 넘어가게 설정되어있습니다.
    inx는 급한대로 dao에 있는 정보 끌어와서 매꿨습니다.
    user mapper 수정있습니다.
    
    * 아이디/비번찾기/ 회원탈퇴 작업중 dao / mapper 수정 있음
    
    아이디/비밀번호찾기 잘못 작성한  mapper 수정했습니다. 수정 시 정상작동 / quitAction 기능 추가 / quit.jsp 아직 미수정
    
    * 로그인기능
    
    서비스에 세션 넘겨서 처리했습니다.
    
    * 로그인서비스 모델삭제 세션적용
    
    * 로그인 컨트롤러 / 로그인 기능 완성
    
    * idFind pwFind
    
    * quit.jsp 수정 / quitAction 완성
    
    * join.jsp 롤백 되돌림
    
    * 회원가입 미완성
    
    mapper를 param 으로 변경없이 동작되게 구현 고민

--- ezen1/src/main/java/com/ezen/service/LoginService.java
+++ ezen1/src/main/java/com/ezen/service/LoginService.java
@@ -1,10 +1,11 @@
 package com.ezen.service;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 
 import com.ezen.dao.IuserDAO;
 
@@ -17,40 +18,37 @@ public class LoginService {
 	IuserDAO userDao;
 	
 	//로그인
-	public Model login(String user_id, String user_pw, HttpSession session, Model model) {
+	public String login(String user_id, String user_pw, HttpSession session) {
 		
 		System.out.println("user_id:"+user_id);
 		System.out.println("user_pw:"+user_pw);
-		
+			
+		String result = "<script>alert('로그인 실패!'); history.back(-1);</script>";
+			
 		// 입력한 id가 있는지 조회
 		String userID = userDao.getUserID(user_id);
 		// 없으면 로그인 실패 로그인페이지 로딩
 		if(userID == null) {
-			return null;
+			return result;
 		}
 		// 있으면 해당ID의 비밀번호 조회
 		String userPw = userDao.getUserPw(user_id);
 		// 입력한 id의 비밀번호와 DB의 비밀번호가 같은지 대조
 		if(user_pw.equals(userPw)) {
-			
+				
 			//유저 idx 불러오기 성공.
 			int user_idx = userDao.getUserIdx(user_id);
-
 			System.out.println("user_idx:"+user_idx);
 			session.setAttribute("user_id", user_id);
 			session.setAttribute("user_idx", user_idx);	
-			
-			model.addAttribute("user_id", user_id);
-			//model.addAttribute("user_pw", user_pw);
-			return model;
+				
+			result = "<script>alert('로그인 성공!'); location.href='/main';</script>";
+			return result;
 		}
-		return null;		
+		return result;									
 	}
 
 	public String findId( String user_name, String user_email) {
-		
-		System.out.println("서비스  user_name:"+user_name);
-		System.out.println("user_email:"+user_email);
 		
 		String result;
 		String user_id = userDao.getUserIdByFindId(user_name, user_email);
@@ -65,10 +63,6 @@ public class LoginService {
 	}
 	
 	public String findPW( String user_id, String user_name, String user_email) {
-		
-		System.out.println("서비스  user_name:"+user_name);
-		System.out.println("user_id:"+user_id);
-		System.out.println("user_email:"+user_email);
 		
 		String result;
 		String user_pw = userDao.getUserPwByFindPw( user_id, user_name, user_email);
@@ -97,11 +91,25 @@ public class LoginService {
 		
 	}
 	
-	public int join( String user_id, String user_pw, String user_name, String user_phone, String user_address, String user_email) {
+	public String join( String user_id, String user_pw, String user_name, String user_phone, String user_address, String user_email, HttpServletRequest request) {
+		String sample6_address = request.getParameter("sample6_address");
+		String sample6_detailAddress = request.getParameter("sample6_detailAddress");
+		user_address = "";
+		if( StringUtils.hasText(sample6_address)) {
+			user_address += sample6_address;
+		}
+		if( StringUtils.hasText(sample6_detailAddress)) {
+			user_address += " " + sample6_detailAddress;
+		}
+		System.out.println("user_address:"+user_address);
 		
-		int result= userDao.insertUser(user_id,  user_pw,  user_name,  user_phone,  user_address,  user_email);
-		System.out.println("result:"+result);
-		return result;
+		int result = userDao.insertUser(user_id, user_pw, user_name, user_phone, user_address,  user_email);
+		if( result == 1 ) {
+			return "<script>alert('회원가입되었습니다.'); location.href='login';</script>";
+		}
+		else {
+			return "<script>alert('회원가입 실패'); history.back(-1);</script>";
+		}
 	}
 
 }
