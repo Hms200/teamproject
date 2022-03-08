02d0f052bdc26ca4da21a243ec58923b073c1601
Jun981020 <flwnsgud@naver.com>
2022-03-08 10:29:14 +0900

    commit

--- ezen1/src/main/java/com/ezen/controller/MyPageController.java
+++ ezen1/src/main/java/com/ezen/controller/MyPageController.java
@@ -1,11 +1,19 @@
 package com.ezen.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.ezen.dao.IreviewDAO;
+import com.ezen.dto.Review;
 
 @Controller
 @RequestMapping("myPage")
 public class MyPageController {
+	
+	@Autowired
+	IreviewDAO reviewDAO;
 
 	@RequestMapping("/memberInfo")
 	public String memberInfo() {
@@ -35,6 +43,14 @@ public class MyPageController {
 	@RequestMapping("/reviewpopup")
 	public String reviewpopup() {
 		return "myPage/reviewpopup";
+	}
+	
+	@RequestMapping("/reviewWriteAction")
+	public String reviewWriteAction(@RequestBody Review review) {
+		
+		int result = reviewDAO.insertReview(review);
+		
+		return "";
 	}
 	
 	
