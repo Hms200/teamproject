c967c15f6574f8a358b1d4a247d9f76bd6fee28d
DevYun97 <yundh812@gmail.com>
2022-03-16 14:41:55 +0900

    로그인서비스 임포트 삭제, 설명주석 추가

--- ezen1/src/main/java/com/ezen/service/LoginService.java
+++ ezen1/src/main/java/com/ezen/service/LoginService.java
@@ -1,6 +1,5 @@
 package com.ezen.service;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,36 +8,29 @@ import org.springframework.stereotype.Service;
 import com.ezen.dao.IuserDAO;
 import com.ezen.dto.User;
 
-
 @Service
 public class LoginService {
-
 
 	@Autowired
 	IuserDAO userDao;
 	
 	//로그인
+	// MALL_USER에 있는 user_id 값 조회 후 null값일 경우 js로 이전 페이지로 이동 
+	// user_id가 있을 경우 user_id과 매치되는 userPw값과 MALL_USER에 있는 userPw값 일치할 경우
+	// session에 user_id와, user_id와 매칭되는 user_idx값 저장. js로 메인페이지로 이동 
 	public String login(String user_id, String user_pw, HttpSession session) {
-		
-		System.out.println("user_id:"+user_id);
-		System.out.println("user_pw:"+user_pw);
 			
 		String result = "<script>alert('로그인 실패!'); history.back(-1);</script>";
-			
-		// 입력한 id가 있는지 조회
+		
 		String userID = userDao.getUserID(user_id);
-		// 없으면 로그인 실패 로그인페이지 로딩
 		if(userID == null) {
 			return result;
 		}
-		// 있으면 해당ID의 비밀번호 조회
+
 		String userPw = userDao.getUserPw(user_id);
-		// 입력한 id의 비밀번호와 DB의 비밀번호가 같은지 대조
 		if(user_pw.equals(userPw)) {
 				
-			//유저 idx 불러오기 성공.
 			int user_idx = userDao.getUserIdx(user_id);
-			System.out.println("user_idx:"+user_idx);
 			session.setAttribute("user_id", user_id);
 			session.setAttribute("user_idx", user_idx);	
 				
@@ -48,50 +40,51 @@ public class LoginService {
 		return result;									
 	}
 
+	//아이디찾기
+	//mapper를 통해 입력받은 user_name와 user_email과 매칭되는 user_id값 조회
+	//null 값일 경우 이전 페이지 이동, 값이 존재할 경우 자바스크립트 alert을 이용해 user_id 전달
 	public String findId( String user_name, String user_email) {
 		
-		String result;
 		String user_id = userDao.getUserIdByFindId(user_name, user_email);
-		System.out.println("user_id:"+user_id);
+		
 		if( user_id == null ) {
-			result = "<script>alert('아이디를 찾을 수 없습니다.'); history.back(-1);</script>";
-			
+			return "<script>alert('아이디를 찾을 수 없습니다.'); history.back(-1);</script>";			
 		} else {
-			result = "<script>alert('고객님의 아이디는" + user_id + " 입니다.'); location.href='login';</script>";;
-		}
-		return result;	
+			return "<script>alert('고객님의 아이디는" + user_id + " 입니다.'); location.href='login';</script>";
+		}	
 	}
 	
+	//비밀번호찾기 
 	public String findPW( String user_id, String user_name, String user_email) {
 		
-		String result;
 		String user_pw = userDao.getUserPwByFindPw( user_id, user_name, user_email);
-		System.out.println("user_pw:"+user_pw);
+
 		if( user_pw == null ) {
-			result = "<script>alert('비밀번호를 찾을 수 없습니다.'); history.back(-1);</script>";
+			return "<script>alert('비밀번호를 찾을 수 없습니다.'); history.back(-1);</script>";
 			
 		} else {
-			result = "<script>alert('고객님의 비밀번호는 " + user_pw + " 입니다.'); location.href='login';</script>";;
+			return "<script>alert('고객님의 비밀번호는 " + user_pw + " 입니다.'); location.href='login';</script>";
 		}
-		return result;	
 	}
 	
 	//아이디 중복확인 구동
+	//MALL_USER에 있는 user_id 값을 조회하여 int값으로 반환 후 
+	// jsp -> main.js 경로를 거쳐 결과값 처리
 	public int idCheckAjax( String user_id ) {
 		
 		int result;	
 		String userID = userDao.getUserID( user_id );
 		if( userID == null ) {
-			result = 0;
-			System.out.println("사용가능한 아이디");						
+			result = 0; //int값이 0일 경우 중복없음 -> 사용가능한 아이디			
 		} else {
-			result = 1;
+			result = 1; //int값이 1일 경우 중복있음 -> 사용불가한 아이디
 		}		
 		return result;
 		
 	}
 	
 	//회원가입 
+	//입력받은 user정보를 User user값에 insert
 	public String join(User user) {
 					
 		int result = userDao.insertUser(user);
@@ -102,4 +95,5 @@ public class LoginService {
 			return "<script>alert('회원가입 실패'); history.back(-1);</script>";
 		}
 	}
+	
 }
